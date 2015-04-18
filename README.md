b96b42e012f1f4e918f063b7e4c6131c348acd70
erikliland <erik.liland@gmail.com>
2015-04-18 13:03:27 +0200

    Released alpha version

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # TTK4145-Simulator
 Simulator of the elevator model at "Sanntidssalen" at NTNU
+
+How to use: 
+The simulator core have the same interface as the driver in this rep. The driver wraps Go-calls to C-calls and imports Comedi.
+
+Recomended way to implement:
+Set up the elevator program on the lab with the driver/wraper. Then edit the import statement from "driver" to "simulator". The simulator should start and self initialise. When you need to button press commands, you need to start the simulatorInterface.go. This program sends buttoncommands from the keyboard to the simulator core. The buttons are mapped from Z->V on internal orders, Q->E for external orders Up, and S->F for external orders Down.
---
+++ simulatorInterface.go
@@ -0,0 +1,91 @@
+package main
+
+// export GOPATH=/home/eriklil/Documents/TTK4145/Project/
+//export GOPATH=/Users/Erik/OneDrive/02\ NTNU/01\ Fag/TTK4145\ Sanntid/TTK4145/Project/
+// export GOPATH=$(pwd)/
+// export GOPATH=~/
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"strconv"
+	. "typedef"
+)
+
+func listenForUserInput(userInput chan string) {
+	for {
+		var command int
+		fmt.Scanf("%c", &command)
+		if (command >= 97) && (command <= 122) {
+			fmt.Println("Sending: ", string(command))
+			userInput <- string(command)
+		} else if command != 10 {
+			fmt.Println("Rejected: ", string(command))
+		}
+	}
+}
+
+func UDPTransmitServer(lconn *net.UDPConn, sendChannel chan string) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("ERROR in UDPTransmitServer.\nClosing connection.")
+			lconn.Close()
+			os.Exit(1)
+		}
+	}()
+	for {
+		msg := <-sendChannel
+		networkPack, err := json.Marshal(msg)
+		if err != nil {
+			log.Println(err)
+		}
+		_, err = lconn.Write(networkPack)
+		if err != nil {
+			fmt.Printf("UDPTransmitServer-Simulator:\tError: Sending\n")
+			panic(err)
+		}
+	}
+}
+
+func main() {
+	fmt.Println("Starting Simulator interface")
+
+	//Generating recive adress
+	raddr, err := net.ResolveUDPAddr("udp4", "localhost:"+strconv.Itoa(PortFromInterface))
+	if err != nil {
+		fmt.Println("Can not resolve this adress")
+		log.Println(err)
+		panic(err)
+	} else {
+		fmt.Printf("Sending to:\t %s\n", raddr)
+	}
+
+	//Creating local connection
+	localTransmitConn, err := net.DialUDP("udp4", nil, raddr)
+	if err != nil {
+		fmt.Println("Can not create UDP soccet on this port")
+		log.Println(err)
+		panic(err)
+	} else {
+		fmt.Println("From:\t\t", localTransmitConn.LocalAddr().String())
+	}
+
+	//Making channels
+	sendChannel := make(chan string, 10)
+	userInput := make(chan string)
+
+	//Spawning threads
+	go UDPTransmitServer(localTransmitConn, sendChannel)
+	go listenForUserInput(userInput)
+
+	for {
+		select {
+		case char := <-userInput:
+			sendChannel <- char
+		}
+	}
+}
