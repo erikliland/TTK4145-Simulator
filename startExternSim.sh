71599271f98f121960c2e0e77b55b041a871a50c
erikliland <erik.liland@gmail.com>
2015-05-06 13:10:57 +0200

    Re-location

---
+++ startExternSim.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+clear
+echo "Type in username:"
+read user
+echo "Type in the last byte of the IP to the elvator you want to connect to:"
+read IP
+echo "Connecting to 129.241.187."$IP
+scp -rq Simulator/. $user@129.241.187.$IP:~/
+ssh $user@129.241.187.$IP
